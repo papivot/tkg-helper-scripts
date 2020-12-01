87f316b2c367644bf00d14f570bf1a4dcf787021
Navneet Verma <47264956+papivot@users.noreply.github.com>
2020-11-30 21:16:31 -0500

    Update create-wcp-privuser.sh

--- create-wcp-privuser.sh
+++ create-wcp-privuser.sh
@@ -1,3 +1,10 @@
-govc sso.user.create -p VMware1! -R Admin wcp-user1
-govc sso.group.update -a=wcp-user1 ServiceProviderUsers
-govc permissions.set -principal='wcp-user1@VSPHERE.LOCAL' -propagate=true -role=Admin /
+# Highly experimantal. Not for production use. Can cause issues !!!!
+# Modify the two variables. 
+export WCP_USER=wcp-user1
+export PASSWORD=VMware1!
+
+govc sso.user.create -p ${PASSWORD} -R Admin ${WCP_USER}
+govc sso.group.update -a=${WCP_USER} ServiceProviderUsers
+
+#The below line may not work. In this case, modify the global permission to add the user with admin role, propagate enabled. 
+govc permissions.set -principal="${WCP_USER}@VSPHERE.LOCAL" -propagate=true -role=Admin /
