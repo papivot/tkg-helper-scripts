7c02e80212d44a9cb82da991e09bf2b9aed41bfa
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-02-09 20:37:05 -0500

    Update create-wcp-privuser.sh

--- create-wcp-privuser.sh
+++ create-wcp-privuser.sh
@@ -8,3 +8,7 @@ govc sso.group.update -a=${WCP_USER} ServiceProviderUsers
 
 #The below line may not work. In this case, modify the global permission to add the user with admin role, propagate enabled. 
 govc permissions.set -principal="${WCP_USER}@VSPHERE.LOCAL" -propagate=true -role=Admin /
+
+# govc tags.category.create iscsi-category
+# govc tags.create -c iscsi-category iscsi
+# govc tags.attach iscsi /RegionA01/datastore/RegionA01-ISCSI02-COMP01
