17d12cf1d3f39e153e83b77e702f1644bea706f4
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-03-10 18:44:54 -0500

    Update get-harbor-admin-password.sh

--- get-harbor-admin-password.sh
+++ get-harbor-admin-password.sh
@@ -1,1 +1,1 @@
-kubectl get secrets -n vmware-system-registry-xxxxxxxx harbor-xxxxxxxx-controller-registry '.data.harborAdminPassword'|base64 -d|base64 -d;echo
+kubectl get secrets -n vmware-system-registry-xxxxxxxx harbor-xxxxxxxx-controller-registry -o json|jq -r '.data.harborAdminPassword'|base64 -d|base64 -d;echo
