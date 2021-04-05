6fa53b3180b7c5c107ac3f5d2e3dea1b7deec0bc
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-04-05 17:02:37 -0400

    Delete modify_ova_size.sh

--- modify_ova_size.sh
+++
@@ -1,6 +0,0 @@
-export tkgrimage="ob-16924026-photon-3-k8s-v1.18.5---vmware.1-tkg.1.c40d30d"
-wget --no-parent -r -nH --cut-dirs=2 --reject="index.html*" https://wp-content.vmware.com/v2/latest/${tkgrimage}/
-cd ${tkgrimage}
-# sed -i Capacty:16 -> Capacity:32
-#sha1sum photon-ova.ovf  and move copy the value to manifest 
-govc library.import -n ${tkgrimage} -m=true Local photon-ova.ovf
