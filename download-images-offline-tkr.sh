bc70052567c73a00470a5403711dd32bdc44ca3f
Navneet Verma <47264956+papivot@users.noreply.github.com>
2021-04-05 16:08:34 -0400

    Update download-images-offline-tkr.sh

--- download-images-offline-tkr.sh
+++ download-images-offline-tkr.sh
@@ -10,16 +10,16 @@ if ! command -v wget >/dev/null 2>&1 ; then
 fi
 
 echo
-echo "The VMware subscribed content library has the following images: "
+echo "The VMware subscribed content library has the following Tanzu Kubernetes Release images ... "
 echo
 curl -s https://wp-content.vmware.com/v2/latest/items.json |jq -r '.items[].name'
 echo
-read -p "Enter the name of the content library that you want to download and zip for offline upload: " tkgrimage
+read -p "Enter the name of the TanzuKubernetesRelease OVA that you want to download and zip for offline upload: " tkgrimage
 echo
-echo Downloading all files for the TKG image: ${tkgrimage}
+echo "Downloading all files for the TKG image: ${tkgrimage} ..."
 echo
 wget -q --show-progress --no-parent -r -nH --cut-dirs=2 --reject="index.html*" https://wp-content.vmware.com/v2/latest/${tkgrimage}/
-echo "Compressing downloaded files"
+echo "Compressing downloaded files..."
 tar -cvzf ${tkgrimage}.tar.gz ${tkgrimage} --totals
 echo
 echo "Cleaning up..."
